c6bdae747fc23e34997751dd56db0bb0e4244fbf
max-allen <contact@maxallen.io>
2023-09-12 10:50:21 -0400

    colocates range tests with src

--- tests/conftest.py
+++ tests/conftest.py
@@ -297,11 +297,6 @@ def integer_lists():
 
 
 @pytest.fixture
-def non_overlapping_ranges():
-    return [[2, 4], [4, 6]]
-
-
-@pytest.fixture
 def parentheses_inputs():
     return [("()", True), ("()[]{}", True), ("(]", False), ("(){", False)]
 
@@ -318,19 +313,3 @@ def sorted_lists_merged():
         "list_two": build_list_from_values([1, 3, 4]),
         "expected": [1, 1, 2, 3, 4, 4],
     }
-
-
-@pytest.fixture
-def sorted_intervals():
-    return [
-        {
-            "existing_intervals": [[1, 3], [6, 9]],
-            "insert_interval": [2, 5],
-            "expected": [[1, 5], [6, 9]],
-        },
-        {
-            "existing_intervals": [[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]],
-            "insert_interval": [4, 8],
-            "expected": [[1, 2], [3, 10], [12, 16]],
-        },
-    ]
